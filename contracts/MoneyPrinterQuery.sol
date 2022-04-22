13630ce9ad6a2bff61a27cc04642c250d5c146cb
Igor Bodnar <bodnar_i@mail.ru>
2022-04-23 01:07:02 +0300

    Optimize contract

--- contracts/MoneyPrinterQuery.sol
+++ contracts/MoneyPrinterQuery.sol
@@ -19,15 +19,18 @@ contract MoneyPrinterQuery {
 
         for (uint i = 0; i < pools.length; i++) {
             result[i] = new uint256[2][](amounts.length);
+            address token0 = pools[i].token0();
+            address token1 = pools[i].token1();
+            uint24 fee = pools[i].fee();
 
             for (uint j = 0; j < amounts.length; j++) {
-                try quoter.quoteExactInputSingle(pools[i].token0(), pools[i].token1(), pools[i].fee(), amounts[j], 0) returns (uint256 amountOut) {
+                try quoter.quoteExactInputSingle(token0, token1, fee, amounts[j], 0) returns (uint256 amountOut) {
                     result[i][j][0] = amountOut;
                 } catch {
                     result[i][j][0] = 0;
                 }
 
-                try quoter.quoteExactInputSingle(pools[i].token1(), pools[i].token0(), pools[i].fee(), amounts[j], 0) returns (uint256 amountOut) {
+                try quoter.quoteExactInputSingle(token1, token0, fee, amounts[j], 0) returns (uint256 amountOut) {
                     result[i][j][1] = amountOut;
                 } catch {
                     result[i][j][1] = 0;
