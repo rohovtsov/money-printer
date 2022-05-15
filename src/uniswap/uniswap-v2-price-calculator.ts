dbf5e8430c32803aca88810fba7de68cb18ad1e8
Yurii Rohovtsov <yurii.rohovtsov@gmail.com>
2022-05-16 02:39:34 +0300

    create uniswapV2 native bigint calculator

--- src/uniswap/uniswap-v2-price-calculator.ts
+++ src/uniswap/uniswap-v2-price-calculator.ts
@@ -2,6 +2,38 @@ import { BigNumber } from 'ethers';
 import { ChainId, Pair, Route, Token, TokenAmount, TradeType, Trade } from '@uniswap/sdk';
 import { PriceCalculator } from '../entities';
 
+export const NativeUniswapV2Calculator = {
+  getTokensIn(reserveIn: bigint, reserveOut: bigint, amountOut: bigint): bigint | null {
+    if (reserveIn <= 0n || reserveOut <= 0n || amountOut >= reserveOut || amountOut <= 0n) {
+      //InsufficientReservesError
+      return null;
+    }
+
+    const numerator = reserveIn * amountOut * 1000n;
+    const denominator = (reserveOut - amountOut) * 997n;
+    return numerator / denominator + 1n;
+  },
+
+  getTokensOut(reserveIn: bigint, reserveOut: bigint, amountIn: bigint): bigint | null {
+    if (reserveIn <= 0 || reserveOut <= 0n || amountIn <= 0n) {
+      //InsufficientReservesError
+      return null;
+    }
+
+    const amountInWithFee = amountIn * 997n;
+    const numerator = amountInWithFee * reserveOut;
+    const denominator = reserveIn * 1000n + amountInWithFee;
+    const outputAmount = numerator / denominator;
+
+    if (outputAmount === 0n) {
+      //InsufficientInputAmountError;
+      return null;
+    }
+
+    return outputAmount;
+  },
+};
+
 export const SimpleUniswapV2Calculator: PriceCalculator = {
   getTokensIn(reserveIn: BigNumber, reserveOut: BigNumber, amountOut: BigNumber): BigNumber | null {
     if (reserveIn.lte(0) || reserveOut.lte(0) || amountOut.gte(reserveOut) || amountOut.lte(0)) {
--- test/uniswap-v2-price-calculator.test.ts
+++ test/uniswap-v2-price-calculator.test.ts
@@ -1,5 +1,6 @@
-import { ETHER } from '../src/entities';
+import { endTime, ETHER, startTime } from '../src/entities';
 import {
+  NativeUniswapV2Calculator,
   SdkUniswapV2Calculator,
   SimpleUniswapV2Calculator,
 } from '../src/uniswap/uniswap-v2-price-calculator';
@@ -8,6 +9,7 @@ import { expect } from 'chai';
 describe('PriceCalculator', function () {
   let uniswapV2Simple = SimpleUniswapV2Calculator;
   let uniswapV2Sdk = SdkUniswapV2Calculator;
+  let native = NativeUniswapV2Calculator;
 
   it('UniswapV2Sdk & UniswapV2Simple - calculate same results', function () {
     const reserves1 = ETHER;
@@ -39,5 +41,85 @@ describe('PriceCalculator', function () {
 
     expect(inputSimple?.toString()).to.equal(inputSdk?.toString());
     expect(outputSimple?.toString()).to.equal(outputSdk?.toString());
+  });
+
+  it('UniswapV2Sdk & Native - calculate same results', function () {
+    const reserves1 = ETHER;
+    const reserves2 = ETHER.mul(2);
+    const output = ETHER.mul(100);
+    const input = ETHER.mul(20);
+
+    const inputSimple = native.getTokensIn(
+      BigInt(reserves1.toString()),
+      BigInt(reserves2.toString()),
+      BigInt(output.toString()),
+    );
+    const inputSdk = uniswapV2Sdk.getTokensIn(reserves1, reserves2, output);
+
+    const outputSimple = native.getTokensOut(
+      BigInt(reserves1.toString()),
+      BigInt(reserves2.toString()),
+      BigInt(input.toString()),
+    );
+    const outputSdk = uniswapV2Sdk.getTokensOut(reserves1, reserves2, input);
+
+    expect(inputSimple?.toString()).to.equal(inputSdk?.toString());
+    expect(outputSimple?.toString()).to.equal(outputSdk?.toString());
+  });
+
+  it('UniswapV2Sdk & UniswapV2Simple - give same errors', function () {
+    const reserves1 = ETHER;
+    const reserves2 = ETHER.mul(2);
+    const output = ETHER.mul(100);
+    const input = ETHER.mul(20);
+
+    const inputSimple = native.getTokensIn(
+      BigInt(reserves1.toString()),
+      BigInt(reserves2.toString()),
+      BigInt(output.toString()),
+    );
+    const inputSdk = uniswapV2Sdk.getTokensIn(reserves1, reserves2, output);
+
+    const outputSimple = native.getTokensOut(
+      BigInt(reserves1.toString()),
+      BigInt(reserves2.toString()),
+      BigInt(input.toString()),
+    );
+    const outputSdk = uniswapV2Sdk.getTokensOut(reserves1, reserves2, input);
+
+    expect(inputSimple?.toString()).to.equal(inputSdk?.toString());
+    expect(outputSimple?.toString()).to.equal(outputSdk?.toString());
+  });
+
+  it('NativeUniswapV2Calculator speedtest', function () {
+    let reserves1 = BigInt(ETHER.toString());
+    const reserves2 = BigInt(ETHER.mul(2).toString());
+    const output = BigInt(ETHER.div(100).toString());
+    const input = BigInt(ETHER.div(20).toString());
+
+    startTime();
+
+    for (let i = 0; i < 100000; i++) {
+      reserves1 += BigInt(i);
+      const outputSimple = native.getTokensIn(reserves1, reserves2, input);
+    }
+
+    console.log(endTime());
+  });
+
+  it('uniswapV2Simple speedtest', function () {
+    let reserves1 = ETHER;
+    const reserves2 = ETHER.mul(2);
+    const output = ETHER.div(100);
+    const input = ETHER.div(20);
+
+    startTime();
+
+    for (let i = 0; i < 100000; i++) {
+      reserves1 = reserves1.add(i);
+      const outputSimple = uniswapV2Simple.getTokensIn(reserves1, reserves2, input);
+    }
+
+    console.log(endTime());
   });
 });
