ff32b042cadf203c92bc780dbd92320109383208
Yurii Rohovtsov <yurii.rohovtsov@gmail.com>
2022-05-22 16:18:54 +0300

    fix: environment

--- src/entities/environmet.ts
+++ src/entities/environmet.ts
@@ -9,10 +9,9 @@ export const PRIVATE_KEY =
   process.env.PRIVATE_KEY || '0xe287672c1f7b7a8a38449626b3303a2ad4430672977b8a6f741a9ca35b6ca10c';
 export const MIN_PROFIT_NET = BigInt(String(Number(process.env.MIN_PROFIT_NET ?? 0) * 10 ** 18));
 
-export const USE_FLASHBOTS = Boolean(process.env.USE_FLASHBOTS ?? true);
-export const FLASHBOTS_RELAY_HACKED_SIGNING_KEY = Boolean(
-  process.env.FLASHBOTS_RELAY_HACKED_SIGNING_KEY ?? true,
-);
+export const USE_FLASHBOTS = (process.env.USE_FLASHBOTS ?? 'true') === 'true';
+export const FLASHBOTS_RELAY_HACKED_SIGNING_KEY =
+  (process.env.FLASHBOTS_RELAY_HACKED_SIGNING_KEY ?? 'false') === 'true';
 export const FLASHBOTS_RELAY_SIGNING_KEY = process.env.FLASHBOTS_RELAY_SIGNING_KEY;
 export const MINER_REWORD_PERCENT = Number(process.env.MINER_REWORD_PERCENT ?? 50);
 export const UNISWAP_V3_GRAPH_ENDPOINT =
