14487192fc5f1b6a4e622880875b226dc71cb481
Igor Bodnar <bodnar_i@mail.ru>
2022-05-26 14:37:00 +0300

    remove fixed size

--- src/reputation.ts
+++ src/reputation.ts
@@ -19,7 +19,11 @@ async function getReputation(flashbotsRelaySigningPrivateKey: string) {
   console.log(`User stats:`, await flashbotsProvider.getUserStats());
 }
 
-async function getBundleStats(flashbotsRelaySigningPrivateKey: string, bundleHash: string) {
+async function getBundleStats(
+  flashbotsRelaySigningPrivateKey: string,
+  bundleHash: string,
+  blockNumber: number,
+) {
   // const provider = new providers.WebSocketProvider('ws://127.0.0.1:8546', NETWORK);
   const provider = new providers.AlchemyWebSocketProvider(NETWORK, ALCHEMY_API_KEY);
   const rpcUrl =
@@ -33,8 +37,12 @@ async function getBundleStats(flashbotsRelaySigningPrivateKey: string, bundleHas
     NETWORK,
   );
 
-  console.log(`User stats:`, await flashbotsProvider.getBundleStats(bundleHash, 14823697));
+  console.log(`User stats:`, await flashbotsProvider.getBundleStats(bundleHash, blockNumber));
 }
 
-getReputation('0xd6a4ffff3bced79e805b077a9110b3be016bb0b1cf5e8dd4b744fb16caac4ee2');
-// getBundleStats('0xd6a4ffff3bced79e805b077a9110b3be016bb0b1cf5e8dd4b744fb16caac4ee2', '0xafb482c135985dcf126a14fd40743afdb8b65f10471a166c19dbcf8fc5634294');
+// getReputation('0xd6a4ffff3bced79e805b077a9110b3be016bb0b1cf5e8dd4b744fb16caac4ee2');
+getBundleStats(
+  '0xd6a4ffff3bced79e805b077a9110b3be016bb0b1cf5e8dd4b744fb16caac4ee2',
+  '0xafb482c135985dcf126a14fd40743afdb8b65f10471a166c19dbcf8fc5634294',
+  14823697,
+);
